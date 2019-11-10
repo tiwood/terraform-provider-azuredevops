9cbbfb524514e1e73d7dda2fef98d02f55f81ca2
Thomas Meckel <tmeckel@users.noreply.github.com>
2019-11-10 11:58:09 +0100

    First implementation of azuredevops\resource_group.go

--- azuredevops/resource_group.go
+++ azuredevops/resource_group.go
@@ -3,12 +3,11 @@ package azuredevops
 import (
 	"fmt"
 
-	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 
-	"github.com/microsoft/azure-devops-go-api/azuredevops/core"
-	"github.com/microsoft/azure-devops-go-api/azuredevops/git"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/graph"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/webapi"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/converter"
 )
 
@@ -40,7 +39,7 @@ func resourceAzureGroup() *schema.Resource {
 				ValidateFunc:  validation.NoZeroValues,
 				Optional:      true,
 				ForceNew:      true,
-				ConflictsWith: []string{ "mail", "displayName" },
+				ConflictsWith: []string{"mail", "displayName"},
 			},
 
 			"mail": {
@@ -48,7 +47,7 @@ func resourceAzureGroup() *schema.Resource {
 				ValidateFunc:  validation.NoZeroValues,
 				Optional:      true,
 				ForceNew:      true,
-				ConflictsWith: []string{ "origin_id", "displayName" },
+				ConflictsWith: []string{"origin_id", "displayName"},
 			},
 
 			"displayName": {
@@ -56,7 +55,7 @@ func resourceAzureGroup() *schema.Resource {
 				ValidateFunc:  validation.NoZeroValues,
 				Optional:      true,
 				ForceNew:      true,
-				ConflictsWith: []string{ "origin_id", "mail" },
+				ConflictsWith: []string{"origin_id", "mail"},
 			},
 
 			"description": {
@@ -109,29 +108,100 @@ func resourceAzureGroup() *schema.Resource {
 func resourceAzureGroupCreate(d *schema.ResourceData, m interface{}) error {
 	clients := m.(*aggregatedClient)
 
-	return resourceAzureGroupRead(d, m)
+	// using: POST https://vssps.dev.azure.com/{organization}/_apis/graph/groups?api-version=5.1-preview.1
+	cga := graph.CreateGroupArgs{}
+	val, b := d.GetOk("scope")
+	if b {
+		cga.ScopeDescriptor = converter.String(val.(string))
+	}
+	val, b = d.GetOk("origin_id")
+	if b {
+		cga.CreationContext = &graph.GraphGroupOriginIdCreationContext{
+			OriginId: converter.String(val.(string)),
+		}
+	} else {
+		val, b = d.GetOk("mail")
+		if b {
+			cga.CreationContext = &graph.GraphGroupMailAddressCreationContext{
+				MailAddress: converter.String(val.(string)),
+			}
+		} else {
+			val, b = d.GetOk("displayName")
+			if b {
+				cga.CreationContext = &graph.GraphGroupVstsCreationContext{
+					DisplayName: converter.String(val.(string)),
+					Description: converter.String(d.Get("description").(string)),
+				}
+			} else {
+				return fmt.Errorf("INTERNAL ERROR: Unable to determine strategy to create group")
+			}
+		}
+	}
+	group, err := clients.GraphClient.CreateGroup(clients.ctx, cga)
+	if err != nil {
+		return err
+	}
+	return flattenGroup(d, group)
 }
 
 func resourceAzureGroupRead(d *schema.ResourceData, m interface{}) error {
 	clients := m.(*aggregatedClient)
 
-	return nil
+	// using: GET https://vssps.dev.azure.com/{organization}/_apis/graph/groups/{groupDescriptor}?api-version=5.1-preview.1
+	// d.Get("descriptor").(string) => {groupDescriptor}
+	getGroupArgs := graph.GetGroupArgs{
+		GroupDescriptor: converter.String(d.Get("descriptor").(string)),
+	}
+	group, err := clients.GraphClient.GetGroup(clients.ctx, getGroupArgs)
+	if err != nil {
+		return err
+	}
+	return flattenGroup(d, group)
 }
 
 func resourceAzureGroupUpdate(d *schema.ResourceData, m interface{}) error {
 	clients := m.(*aggregatedClient)
 
-	return resourceAzureGroupRead(d, m)
+	// using: PATCH https://vssps.dev.azure.com/{organization}/_apis/graph/groups/{groupDescriptor}?api-version=5.1-preview.1
+	// d.Get("descriptor").(string) => {groupDescriptor}
+
+	group, err := expandGroup(d)
+	if err != nil {
+		return err
+	}
+	// as specified in the schema, the only updatable attribute is the description
+
+	uptGroupArgs := graph.UpdateGroupArgs{
+		GroupDescriptor: converter.String(d.Get("descriptor").(string)),
+		PatchDocument: &[]webapi.JsonPatchOperation{
+			{
+				Op:    &webapi.OperationValues.Replace,
+				From:  nil,
+				Path:  converter.String("/description"),
+				Value: group.Description,
+			},
+		},
+	}
+	group, err = clients.GraphClient.UpdateGroup(clients.ctx, uptGroupArgs)
+	if err != nil {
+		return err
+	}
+	return flattenGroup(d, group)
 }
 
 func resourceAzureGroupDelete(d *schema.ResourceData, m interface{}) error {
 	clients := m.(*aggregatedClient)
 
-	return nil
+	// using: DELETE https://vssps.dev.azure.com/{organization}/_apis/graph/groups/{groupDescriptor}?api-version=5.1-preview.1
+	// d.Get("descriptor").(string) => {groupDescriptor}
+	delGroupArgs := graph.DeleteGroupArgs{
+		GroupDescriptor: converter.String(d.Get("descriptor").(string)),
+	}
+	return clients.GraphClient.DeleteGroup(clients.ctx, delGroupArgs)
 }
 
 // Convert internal Terraform data structure to an AzDO data structure
-func expandGroup(clients *aggregatedClient, d *schema.ResourceData, forCreate bool) (*graph.GraphGroup, error) {
+func expandGroup(d *schema.ResourceData) (*graph.GraphGroup, error) {
 
 	group := &graph.GraphGroup{
 		Descriptor:    converter.String(d.Get("descriptor").(string)),
@@ -140,7 +210,7 @@ func expandGroup(clients *aggregatedClient, d *schema.ResourceData, forCreate bo
 		Origin:        converter.String(d.Get("origin").(string)),
 		OriginId:      converter.String(d.Get("origin_id").(string)),
 		SubjectKind:   converter.String(d.Get("subject_kind").(string)),
- 		Domain:        converter.String(d.Get("domain").(string)),
+		Domain:        converter.String(d.Get("domain").(string)),
 		MailAddress:   converter.String(d.Get("mail").(string)),
 		PrincipalName: converter.String(d.Get("principal_name").(string)),
 		Description:   converter.String(d.Get("description").(string)),
@@ -149,7 +219,7 @@ func expandGroup(clients *aggregatedClient, d *schema.ResourceData, forCreate bo
 	return group, nil
 }
 
-func flattenGroup(clients *aggregatedClient, d *schema.ResourceData, group *graph.GraphGroup) error {
+func flattenGroup(d *schema.ResourceData, group *graph.GraphGroup) error {
 
 	d.SetId(*group.Descriptor)
 	d.Set("descriptor", *group.Descriptor)
@@ -160,7 +230,7 @@ func flattenGroup(clients *aggregatedClient, d *schema.ResourceData, group *grap
 	d.Set("subject_kind", *group.SubjectKind)
 	d.Set("domain", *group.Domain)
 	d.Set("mail", *group.MailAddress)
-	d.Set("principal_name", *group.PrincipalName
+	d.Set("principal_name", *group.PrincipalName)
 	d.Set("description", *group.Description)
 
 	return nil
