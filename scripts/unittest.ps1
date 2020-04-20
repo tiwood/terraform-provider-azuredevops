c84f0a578295977d65be7aabbb9fc859d349461a
Thomas Meckel <tmeckel@users.noreply.github.com>
2020-04-20 22:54:37 +0200

    Squashed commit of the following:
    commit 56a67c93935fa67c351a729f464831f44059c747
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Thu Dec 12 19:41:00 2019 +0100
    
        Added -DebugBuild parameter to scripts\build.ps1
    
    commit 6ab1f0d92de92fc6a6e800689ea80a93f97f694e
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Thu Dec 5 11:26:25 2019 +0100
    
        Added validation to -Tags parameter in scripts\acctest.ps1
    
    commit 6e7544b6c0d5a02235fcbc199d2a4e8bb7293a0f
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Thu Dec 5 11:25:50 2019 +0100
    
        Added same parameters to scripts\unittest.ps1 as scripts\acctest.ps1
    
    commit 731840796fe1a8c3d46e512dbab2bb0a82fd636d
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Thu Nov 28 12:56:26 2019 +0100
    
        Initial check in of scripts\acctest.ps1 and added tags support in scripts\unittest.ps1
    
    commit adbcba737ed1a6a9c25cbb7bb133cee849ea6940
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Thu Nov 28 12:55:37 2019 +0100
    
        Fixed a spelling error of System namespace in scripts\local-install.ps1
    
    commit b8905f9a91171c4b6194ee471fd0180087812a70
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Sun Nov 10 19:59:52 2019 +0100
    
        Added parameter -Install scripts\build.ps1
    
    commit 6c87ab45a1997d43ed385f5e76dc263125e11d8b
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Sun Nov 10 17:56:56 2019 +0100
    
        scripts\local-install.ps1 now installes provider on Windows correctly
    
    commit 02741237bf1cd6b9749349bf49407bef14f827f2
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Sun Nov 10 17:35:41 2019 +0100
    
        scripts\build.ps1 will add .exe extension on Windows
    
    commit 002338a6f65b8b8b906a9e87df823a85b38fdf10
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Sun Nov 10 14:56:52 2019 +0100
    
        Initial check in scripts\local-install.ps1
    
    commit 367e762869cb6736c597af55ca73d7bad2f80a58
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Sun Nov 10 14:23:17 2019 +0100
    
        Fixed trailing whitespaces for scripts\build.ps1 and scripts\unittest.ps1
    
    commit fe69eb89ebdc3de01382e3991b8f4511714d8e37
    Author: Thomas Meckel <14177833+tmeckel@users.noreply.github.com>
    Date:   Fri Nov 8 18:01:25 2019 +0100
    
        Added parameter -SkipTests to scripts\build.ps1
    
    commit cd08bf5a872cd5af322a15f02cd6d7d3feb0f382
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Sat Nov 2 15:27:37 2019 +0100
    
        Updated Powershell build scripts
    
    commit 734f9d3a561c3fee4d7b4c01bc77424faf7d210e
    Author: Thomas Meckel <tmeckel@users.noreply.github.com>
    Date:   Fri Nov 1 16:42:13 2019 +0100
    
        Added initial versions of PowerShell script to build the provider

---
+++ scripts/unittest.ps1
@@ -0,0 +1,41 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $TestFilter,
+
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+    [string[]]
+    $Tag = 'all'
+)
+
+$script:PSDefaultParameterValues = @{
+    '*:Confirm'           = $false
+    '*:ErrorAction'       = 'Stop'
+}
+
+. (Join-Path -Path $PSScriptRoot -ChildPath 'commons.ps1' -Resolve)
+
+Write-Host "Executing unit tests"
+Push-Location -Path $SOURCE_DIR
+try {
+    $argv = @(
+        'test',
+        '-v'
+    )
+    if ($TestFilter) {
+        $argv += @('-run', $TestFilter)
+    }
+    if ($Tag -and 0 -lt $Tag.Length) {
+        $argv += @('-tags', [string]::Join(' ', $Tag))
+    }
+    go @argv ./...
+    if ($LASTEXITCODE) {
+        throw "Build finished in error due to failed tests"
+    }
+}
+finally {
+    Pop-Location
+}
